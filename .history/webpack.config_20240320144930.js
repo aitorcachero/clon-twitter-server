fabbaf7bbcaab26fad2c75425d87f46e817d6088
Aitor Cachero <aitor.cachero@gmail.com>
2024-03-20 19:30:23 +0100

    Prueba

---
+++ .history/webpack.config_20240320144930.js
@@ -0,0 +1,14 @@
+import path from 'path';
+
+const config = {
+  mode: 'production',
+  entry: 'src/app.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'final.js',
+  },
+  target: 'node',
+};
+
+export default config;
---
+++ .history/webpack.config_20240320150104.mjs
@@ -0,0 +1,19 @@
+import path from 'path';
+
+import * as url from 'url';
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+console.log(__dirname);
+
+const config = {
+  mode: 'production',
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'final.js',
+  },
+  target: 'node',
+};
+
+export default config;
