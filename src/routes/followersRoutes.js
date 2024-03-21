35b2ad6a8a7359c2d67f7f4429ae1aee69c075d4
Aitor Cachero <aitor.cachero@gmail.com>
2024-03-21 22:54:54 +0100

    prueba

--- src/routes/followersRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import authUser from '../middlewares/authUser.js';
-import followersController from '../controllers/followersController.js';
-
-const router = express.Router();
-
-router.post('/', authUser, followersController().followFunction);
-
-export default router;
